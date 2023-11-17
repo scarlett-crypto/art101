479c56ccec88714b9f4b59b5c1fadad2b5f9b798
scarlett-crypto <scarlettenove@gmail.com>
2023-11-16 17:28:50 -0800

    lab10

--- Lab10/lab.js
+++ Lab10/lab.js
@@ -1,13 +1,21 @@
+/*
+   lab.js - This simple JavaScript/jQuery script appends new elements to an output div
+
+   Requirements: jQuery must be loaded for this script to work.
+
+   Author: Scarlett Orkin
+   Date: 2023
+*/
 function generateRandomText() {
-    const text = "blah blah blah.";
-    const min = 3;
-    const max = 200;
-    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
-    // Get a random starting index to slice the Lorem Ipsum text
-    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
-    // Generate the random Lorem Ipsum-like text
-    return text.slice(randStart, randStart + randLen);
-  }
+  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  const min = 3;
+  const max = 100;
+  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  // Get a random starting index to slice the Lorem Ipsum text
+  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+  // Generate the random Lorem Ipsum-like text
+  return text.slice(randStart, randStart + randLen);
+}
   
   // click listener for button
   $("#make-convo").click(function(){
