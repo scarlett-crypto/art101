9bd8eb2b1cfffdb39d99689b698d0194dd60aabf
scarlett-crypto <scarlettenove@gmail.com>
2023-11-28 11:22:51 -0800

    fixedlab5

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,21 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5 
+// Author: Scarlett Orkin
+// Date:Nov 10 REDO
 
 // Constants
 
-// Functions
+//define varibles 
+make = "Honda";
+model = "CRV";
+color = "blue";
+year = "2008";
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+//calculate
+age = 2023 - year;
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//output
+document.writeln("make: " make + "<br>");
+document.writeln("model: " model + "<br>");
+document.writeln("color: " color + "<br>");
+document.writeln("year: " year + "<br>");
+document.writeln("age: " age + "years<br>");
