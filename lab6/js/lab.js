26d0e4f4966a73e0d1cf9b4d21cbc9ff129de803
scarlett-crypto <scarlettenove@gmail.com>
2023-11-30 20:53:32 -0800

    o

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,25 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - arrays
+// Author: Scarlett Orkin
+// Date: Nov 26
 
-// Constants
+// Define variabbles
+myTransport = ["honda crv"]
 
-// Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
+//object
 main();
+var myMainRide = {
+  make: "honda",
+  model: "crv",
+  color: "blue",
+  year: "2008",
+  age: function () {
+      return 2023 - this.year;
+  }
+}
+myMainRide.age
+
+// output
+document.writeln("kinds of transportation I use:", myTransport,"</br>");
+document.writeln("myMainRide: <pre",
+JSON.stringify(myMainRibe, null, '\t'), "</pre>");
